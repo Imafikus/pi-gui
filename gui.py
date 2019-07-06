7fdb54c8e0b0affcc6bcb12e6577839c1a9d0616
Aleksa Tesic <tesic@getpenta.com>
2019-07-06 23:34:41 +0200

    Rewrite to pack

--- gui.py
+++ gui.py
@@ -28,52 +28,65 @@ class houseGUI:
         #? NOTE: all variables which are self.sth are that way on purpose, because 
         #? we need to access that data in the start simulation function
 
-        #? time_period widget
+        #? pin entry widget
+        pin_entry_frame = Frame(self.root)
         self.pin_entry_txt = StringVar()
         self.pin_entry_txt.set(PLACEHOLDER_TEXT)
-        pin_entry = ttk.Entry(self.root, textvariable = self.pin_entry_txt)
-
-        pin_entry.grid(row = 0, column = 0, columnspan = 3,  sticky = "nwse", padx = 5, pady = 5)
+        pin_entry = ttk.Entry(pin_entry_frame, textvariable = self.pin_entry_txt)
+        pin_entry.pack(side=TOP, fill = BOTH, expand = TRUE)
+        pin_entry_frame.pack(side=TOP, fill = BOTH, expand = TRUE)
         
         #? entry button widgets
         
-        btn1 = ttk.Button(self.root, text = "1", command = lambda: self.input(1))
-        btn1.grid(row = 1, column = 0, sticky = "nwse", padx = 5, pady = 5)
-        
-        btn2 = ttk.Button(self.root, text = "2", command = lambda: self.input(2))
-        btn2.grid(row = 1, column = 1, sticky = "nwse", padx = 5, pady = 5)
-        
-        btn3 = ttk.Button(self.root, text = "3", command = lambda: self.input(3))
-        btn3.grid(row = 1, column = 2, sticky = "nwse", padx = 5, pady = 5)
-        
-        btn4 = ttk.Button(self.root, text = "4", command = lambda: self.input(4))
-        btn4.grid(row = 2, column = 0, sticky = "nwse", padx = 5, pady = 5)
-        
-        btn5 = ttk.Button(self.root, text = "5", command = lambda: self.input(5))
-        btn5.grid(row = 2, column = 1, sticky = "nwse", padx = 5, pady = 5)
-        
-        btn6 = ttk.Button(self.root, text = "6", command = lambda: self.input(6))
-        btn6.grid(row = 2, column = 2, sticky = "nwse", padx = 5, pady = 5)
+        btn123_frame = Frame(self.root)
+        btn123_frame.pack(side=TOP, fill = BOTH, expand = TRUE)
 
-        btn7 = ttk.Button(self.root, text = "7", command = lambda: self.input(7))
-        btn7.grid(row = 3, column = 0, sticky = "nwse", padx = 5, pady = 5)
+        btn1 = ttk.Button(btn123_frame, text = "1", command = lambda: self.input(1))
+        btn1.pack(side = LEFT, fill = BOTH, expand = TRUE)
+
+        btn2 = ttk.Button(btn123_frame, text = "2", command = lambda: self.input(2))
+        btn2.pack(side = LEFT, fill = BOTH, expand = TRUE)
         
-        btn8 = ttk.Button(self.root, text = "8", command = lambda: self.input(8))
-        btn8.grid(row = 3, column = 1, sticky = "nwse", padx = 5, pady = 5)
+        btn3 = ttk.Button(btn123_frame, text = "3", command = lambda: self.input(3))
+        btn3.pack(side = LEFT, fill = BOTH, expand = TRUE)
+
+        btn456_frame = Frame(self.root)
+        btn456_frame.pack(side=TOP, fill = BOTH, expand = TRUE)
+
+        btn4 = ttk.Button(btn456_frame, text = "4", command = lambda: self.input(4))
+        btn4.pack(side = LEFT, fill = BOTH, expand = TRUE)
         
-        btn9 = ttk.Button(self.root, text = "9", command = lambda: self.input(9))
-        btn9.grid(row = 3, column = 2, sticky = "nwse", padx = 5, pady = 5)
+        btn5 = ttk.Button(btn456_frame, text = "5", command = lambda: self.input(5))
+        btn5.pack(side = LEFT, fill = BOTH, expand = TRUE)
         
-        btn0 = ttk.Button(self.root, text = "0", command = lambda: self.input(0))
-        btn0.grid(row = 4, column = 1, sticky = "nwse", padx = 5, pady = 5)
+        btn6 = ttk.Button(btn456_frame, text = "6", command = lambda: self.input(6))
+        btn6.pack(side = LEFT, fill = BOTH, expand = TRUE)
+
+        btn789_frame = Frame(self.root)
+        btn789_frame.pack(side=TOP, fill = BOTH, expand = TRUE)
+
+        btn6 = ttk.Button(btn789_frame, text = "6", command = lambda: self.input(6))
+        btn6.pack(side = LEFT, fill = BOTH, expand = TRUE)
         
+        btn8 = ttk.Button(btn789_frame, text = "8", command = lambda: self.input(8))
+        btn8.pack(side = LEFT, fill = BOTH, expand = TRUE)
+        
+        btn9 = ttk.Button(btn789_frame, text = "9", command = lambda: self.input(9))
+        btn9.pack(side = LEFT, fill = BOTH, expand = TRUE)
+                
+        bottom_frame = Frame(self.root)
+        bottom_frame.pack(side=TOP, fill = BOTH, expand = TRUE)
+
         #? lock button widget
-        lock_button = ttk.Button(self.root, text = "Lock", command = self.lock)        
-        lock_button.grid(row = 4, column = 0, sticky = "nwse", padx = 5, pady = 5)
+        lock_button = ttk.Button(bottom_frame, text = "Lock", command = self.lock)        
+        lock_button.pack(side = LEFT, fill = BOTH, expand = TRUE)
+
+        btn0 = ttk.Button(bottom_frame, text = "0", command = lambda: self.input(0))
+        btn0.pack(side = LEFT, fill = BOTH, expand = TRUE)
 
         #? unlock button widget
-        unlock_button = ttk.Button(self.root, text = "Unlock", command = self.unlock)
-        unlock_button.grid(row = 4, column = 2, sticky = "nwse", padx = 5, pady = 5)
+        unlock_button = ttk.Button(bottom_frame, text = "Unlock", command = self.unlock)
+        unlock_button.pack(side = LEFT, fill = BOTH, expand = TRUE)
 
     def run(self):
         self.root.mainloop()
