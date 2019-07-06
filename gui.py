d247a508751a823fa14c81cef5c6c9c03bd776b4
Aleksa Tesic <tesic@getpenta.com>
2019-07-06 23:49:56 +0200

    Set display

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ class houseGUI:
 
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("800x480")
+        self.root.geometry("576x416")
         self.root.title("Alarm")
         # self.root.attributes('-fullscreen', True)
 
